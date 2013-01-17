ef3766bfa0439b56ae8af14a6e27a9d3b32ce2ef
SHIOYA, Hiromu <kwappa.856@gmail.com>
2013-01-17 15:31:09 +0900

    first commit
     * create app
     * execute bundle install
     * add /vendor/bundle to .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'cd49cb255782e65a325ec31d561d93ce52cb8b0a8fc6b081a0713978e320f2024b5f2b82af37b36177f73d4a0d5a180a87f530b5ebebd8c2da4a84c35456cda6'
